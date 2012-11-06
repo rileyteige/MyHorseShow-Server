f710a115ea6c6ce6f5ba71f097457df8e20ac755
Riley Teige <riley.teige@gmail.com>
2012-11-06 04:12:58 +0000

    Renaming nuser.php to newuser.php

---
+++ newuser.php
@@ -0,0 +1,42 @@
+<?php
+
+require('setup.php');
+
+function verify_user($user) {
+	return $user->email != null &&
+		$user->firstname != null &&
+		$user->lastname != null &&
+		$user->password != null;
+}
+
+function getUserByEmail($email) {
+	return R::findOne(USER, ' email = ? ', array ( $email ));
+}
+
+$email_address = $_GET[EMAIL_ADDR_PARAM];
+$firstname = $_GET[FNAME_PARAM];
+$lastname = $_GET[LNAME_PARAM];
+$password = $_GET[PASSWORD_PARAM];
+$usef_id = $_GET[USEF_ID_PARAM];
+
+$user = R::dispense(USER);
+$user->email = $email_address;
+$user->firstname = $firstname;
+$user->lastname = $lastname;
+$user->password = $password;
+$user->usefid = $usef_id;
+
+if (!verify_user($user))
+	return;
+	
+if (getUserByEmail($email_address) != null)
+	throw new Exception('Email already registered.');
+	
+$id = R::store($user);
+$rb_user = R::load(USER, $id);
+
+echo json_encode(array(USER_ID => $rb_user->id, USER_FNAME => $rb_user->firstname, USER_LNAME => $rb_user->lastname));
+
+R::close();
+
+?>
